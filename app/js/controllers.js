8f2e8583b7d444f231b7313ffd657ee358e39942
Brett Lyons <blyons@students.naropa.edu>
2015-11-17 11:48:02 -0700

    app ready to test for phone info

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,23 @@
-app.controller('myController', function($scope) {
+app.controller('PhoneController', function($scope) {
   $scope.greeting = "Hello World!";
+  $scope.shakeCount;
+  $scope.xAcceleration = 0;
+  $scope.yAcceleration = 0
+  $scope.zAcceleration = 0;
+
+  window.addEventListener('devicemotion', handleDeviceAccelChange, true);
+  
+  function handleDeviceAccelChange(event) {
+    $scope.accelEventData = event;
+
+    // we use to determine accel: event.acceleration;
+    var xAccel = event.acceleration.x;
+    var yAccel = event.acceleration.y;
+    var zAccel = event.acceleration.z;
+
+    // Accels are all in is in m/s^2
+    $scope.xAcceleration = xAccel;
+    $scope.yAcceleration = yAccel;
+    $scope.zAcceleration = zAccel;
+  }
 });
