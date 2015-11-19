b1fa3baef70f0f406905d34ae308998be3c1b630
Brett Lyons <blyons@students.naropa.edu>
2015-11-19 13:03:40 -0700

    Updated controllers to update players with info from phone

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,7 +12,7 @@ app.controller('PhoneController', function($scope) {
   // });
 
   $scope.emitShake = function(data) {
-    return socket.emit('shake', data);
+    return $scope.socket.emit('shake', data);
   };
 
 
@@ -31,15 +31,17 @@ app.controller('PhoneController', function($scope) {
     $scope.$apply(); 
   };
   window.addEventListener('devicemotion', $scope.handleDeviceAccelChange, true);
+  
 }).controller('PlayGridController', function($scope) {
   if(!$scope.socket) { $scope.socket = io(); }
   // players are hardcoded for now, eventually this will be aggregated from somewhere else
   // so it's sort of a stub that will be adjusted later
-  $scope.players = [{name: 'Bob', score: 5}, {name: 'Fred', score: 7}, {name: 'Jenny', score: 4}]
+  // $scope.players = [{name: 'Bob', score: 5}, {name: 'Fred', score: 7}, {name: 'Jenny', score: 4}]
 
-  socket.on('moveracer', function (data) {
+  $scope.socket.on('moveracer', function (data) {
     // change the 1 object in the array that matches (data)
-    $scope.players = [{name: "Tester Testington", score: data.shakes, userId: data.userId }]; // test data
+    $scope.players = [{name: "Tester Testington", score: data.shakes}]; // DEBUG DEBUG DEBUG
+    // $scope.players = [{name: "Tester Testington", score: data.shakes, userId: data.userId }]; // test data
     $scope.apply();
   });
   
