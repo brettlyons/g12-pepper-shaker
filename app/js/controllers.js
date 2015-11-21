f2b2479d838f726d1545b6610a380a891a534f73
Brett Lyons <blyons@students.naropa.edu>
2015-11-21 13:37:51 -0700

    De-linting, dental cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,50 +1,54 @@
 const ACCELERATION_REPORT_MIN = 12;
-const SHAKES_TO_WIN = 1000;
-const maracas = new Audio('../../Maracas2.mp3');
+const SHAKES_TO_WIN = 100;
 
 app.controller('PhoneController', function($scope, $location, $rootScope) {
-  if(!maracas) { const maracas = new Audio('../../Maracas2.mp3'); }
+  $rootScope.maracas = new Audio('../../Maracas2.mp3');
   $scope.socket = io();
   $scope.shakes = 0;
   $scope.viewDashboard = false;
   $scope.showDash = function() {
-    $scope.viewDashboard = (!$scope.viewDashboard ) ? true : false;
-  }
+    $scope.viewDashboard = (!$scope.viewDashboard) ? true : false;
+  };
 
   $scope.validatePassword = function() {
     if ($scope.password !== '********') {
-      return $location.url('/')
+      return $location.url('/');
     } else {
-      return $location.url('/playgrid')
+      return $location.url('/playgrid');
     }
   };
 
-  maracas.play();
+  $rootScope.maracas.play();
 
-
-  $scope.resetShakes = function () {
+  $scope.resetShakes = function() {
     $scope.shakes = 0;
   };
-  
+
   $scope.addName = function() {
 
-    if ($scope.name.toLowerCase().split(' ').join('').indexOf('dickbutt') >= 0) {
+    if ($scope.name
+        .toLowerCase()
+        .split(' ')
+        .join('')
+        .indexOf('dickbutt') >= 0) {
       $scope.name = 'Prissy spice';
       $location.url('/race');
-    } else if ($scope.name.toLowerCase().split(' ').join('').indexOf('cena') >= 0 || $scope.name == '') {
+    } else if ($scope.name
+               .toLowerCase()
+               .split(' ')
+               .join('')
+               .indexOf('cena') >= 0 || $scope.name == '') {
       $scope.name = 'Posh Spice';
       $location.url('/race');
     }
-    
+
     $rootScope.personName = $scope.name;
-    $location.url('/race')
-
-  }
-
-  $scope.emitShake = function(data) {
-    return $scope.socket.emit('shake', data);
+    $location.url('/race');
   };
 
+  $scope.emitShake = function(data) {
+    $scope.socket.emit('shake', data);
+  };
 
   $scope.handleDeviceAccelChange = function(event) {
     event.preventDefault();
@@ -53,51 +57,54 @@ app.controller('PhoneController', function($scope, $location, $rootScope) {
       event.acceleration.y +
       event.acceleration.z;
 
-   if($scope.totalAcceleration > 20) {
+    if ($scope.totalAcceleration > 20) {
       $scope.shakes++;
 
-      maracas.play();
+      $rootScope.maracas.play();
 
       $scope.emitShake({
         name: $rootScope.personName,
         shakes: $scope.shakes
-      }); 
+      });
       $scope.newsFromSocket = $scope.totalAcceleration;
     }
 
-    $scope.$apply(); 
+    $scope.$apply();
   };
   window.addEventListener('devicemotion', $scope.handleDeviceAccelChange, true);
 
-  $scope.socket.on('reset', function (data) {
+  $scope.socket.on('reset', function() {
     $scope.resetShakes();
   });
-  
-}).controller('PlayGridController', function($scope, $rootScope) {
+
+}).controller('PlayGridController', function($scope) {
   $scope.title = 'SHAKE RACE!!!';
   $scope.socket = io();
 
   if (!$scope.players) { $scope.players = {}; }
-  $scope.activateSocket = function () {
+  $scope.activateSocket = function() {
     const bugle = new Audio('http://static1.grsites.com/archive/sounds/recreation/recreation002.mp3');
     bugle.play();
     $scope.socket.on('moveracer', $scope.racerMover);
     $scope.socket.emit('reset', {});
   };
-  
+
   $scope.deactivateSocket = function() {
     $scope.socket.emit('reset', {});
     $scope.socket.removeAllListeners('moveracer');
   };
   $scope.associateImageWithUniqueId = function(uniqueId) {
     const images = ['/images/pony1.png',
-                  '/images/pony2.png',
-                  '/images/pony3.png'];
+                    '/images/pony2.png',
+                    '/images/pony3.png'];
 
     if (!$scope.imgCache) { $scope.imgCache = {}; }
-    if (!$scope.imgCache[uniqueId]) {$scope.imgCache[uniqueId] = images[Math.floor(Math.random() * (images.length - 1) + 0)]; }
-    
-    console.log("associate image function hit");
+    if (!$scope.imgCache[uniqueId]) {
+      $scope.imgCache[uniqueId] =
+        images[Math.floor(Math.random() * (images.length - 1) + 0)];
+    }
+
+    console.log('associate image function hit');
   };
 
   $scope.racerMover = function(data) {
@@ -110,10 +117,9 @@ app.controller('PhoneController', function($scope, $location, $rootScope) {
       image: $scope.imgCache[data.userId]
     };
 
-
     $scope.$apply();
 
-    if ($scope.players[data.userId].score > 100 ) { // 100 is for 100% width on the DOM
+    if ($scope.players[data.userId].score > SHAKES_TO_WIN) { // 100 is for 100% width on the DOM
       $scope.winner = $scope.players[data.userId].name;
       $scope.deactivateSocket();
       $scope.$apply();
